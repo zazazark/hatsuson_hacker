44201d1f910ca12d66383c95cd194f9d65a19bd5
zazazark <yamazaki_daichi@uniquevision.co.jp>
2021-09-25 10:29:01 +0900

    namespace作成

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require 'json'
 require 'open-uri'
 
-  get '/' do
+  get '/get' do
     res = OpenURI.open_uri('http://weather.livedoor.com/forecast/webservice/json/v1?city=471010')
     res
   end
